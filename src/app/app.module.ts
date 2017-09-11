1d87467ff08138c6b441cae1484794f1461a2e12
QUICK APPS STUDIOS <quickappsstudios@QUICKs-iMac.local>
2017-09-11 20:06:27 +0100

    login and registration complete with firebase setup

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,3 +1,6 @@
+import { RegisterPage } from './../pages/register/register';
+
+import { LoginPage } from './../pages/login/login';
 import { DetailsPage } from './../pages/details/details';
 import { BrowserModule } from '@angular/platform-browser';
 import { ErrorHandler, NgModule } from '@angular/core';
@@ -7,29 +10,56 @@ import { StatusBar } from '@ionic-native/status-bar';
 
 import { MyApp } from './app.component';
 import { HomePage } from '../pages/home/home';
+import { ServicesAuthProvider } from '../providers/services-auth/services-auth';
 
+
+//AngularFire
+
+import{AngularFireModule} from 'angularfire2';
+import{AngularFireDatabaseModule} from 'angularfire2/database';
+
+
+const firebase ={
+
+  apiKey: "AIzaSyCQ2q70bG5jcuQjnlgQq7jr7s86oOTFF4U",
+  authDomain: "test-66e46.firebaseapp.com",
+  databaseURL: "https://test-66e46.firebaseio.com",
+  projectId: "test-66e46",
+  storageBucket: "test-66e46.appspot.com",
+  messagingSenderId: "457540627637"
+
+
+
+}
 
 
 @NgModule({
   declarations: [
     MyApp,
     HomePage,
-    DetailsPage
+    DetailsPage,
+    LoginPage,
+    RegisterPage
   ],
   imports: [
     BrowserModule,
-    IonicModule.forRoot(MyApp)
+    IonicModule.forRoot(MyApp),
+    AngularFireModule.initializeApp(firebase),
   ],
   bootstrap: [IonicApp],
   entryComponents: [
     MyApp,
     HomePage,
-    DetailsPage
+    DetailsPage,
+    LoginPage,
+    RegisterPage
   ],
   providers: [
     StatusBar,
     SplashScreen,
-    {provide: ErrorHandler, useClass: IonicErrorHandler}
+    ServicesAuthProvider,
+    {provide: ErrorHandler, useClass: IonicErrorHandler},
+    ServicesAuthProvider
   ]
 })
 export class AppModule {}
---
+++ src/pages/login/login.ts
@@ -0,0 +1,30 @@
+
+/*Import Login Page*/
+
+
+import { Component } from '@angular/core';
+import { IonicPage, NavController, NavParams } from 'ionic-angular';
+
+//import Registerpage
+import { RegisterPage } from './../register/register';
+
+
+@IonicPage()
+@Component({
+  selector: 'page-login',
+  templateUrl: 'login.html',
+})
+export class LoginPage {
+
+  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  }
+
+  ionViewDidLoad() {
+    console.log('ionViewDidLoad LoginPage');
+  }
+
+  createAccount(){
+  this.navCtrl.push(RegisterPage);
+  }
+
+}
